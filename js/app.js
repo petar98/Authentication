ab6bc325d912a74dec25accc47e69565f70ff88e
petar98 <petaryanakiev.py@gmail.com>
2016-11-18 23:46:44 +0200

    Kinvey connected!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
-var app = angular.module('loginApp', ['ngRoute']);
+var initialized = false;
+var app = angular.module('loginApp', ['ngRoute', 'kinvey']);
 app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({
         enabled: true,
@@ -17,3 +18,19 @@ app.config(function ($routeProvider, $locationProvider) {
     });
     $routeProvider.otherwise({redirectTo: '/'});
 });
+
+app.config(['$kinveyProvider', function($kinveyProvider) {
+    $kinveyProvider.init({
+        appKey: 'kid_HkruFRn-x',
+        appSecret: 'afa88e50a33447a8accfda2707207316'
+    });
+}]);
+
+app.run(['$kinvey', function($kinvey) {
+    var promise = $kinvey.ping();
+    promise.then(function(response) {
+        console.log('Kinvey Ping Success. Kinvey Service is alive, version: ' + response.version + ', response: ' + response.kinvey);
+    }, function(error) {
+        console.log('Kinvey Ping Failed. Response: ' + error.description);
+    });
+}]);
